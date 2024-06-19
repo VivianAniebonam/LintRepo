0f45801f7ea7cadbe35e9e1e564a81f17be88fca
Vivian Aniebonam <viviananiebonam247@gmail.com>
2024-06-18 22:08:07 -0400

    Update Vex_Talent.py

--- Vex_Talent.py
+++ Vex_Talent.py
@@ -1,8 +1,10 @@
-def example_function():
+def example_function():   
+
    
-    x = 10  # Proper indentation and use of variables
+   x = 10  # Proper indentation and use of variables
 
     if x == 10:
         print("Value is 10")  # Proper indentation and line length
+
 
 example_function()
